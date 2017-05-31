dafed683b3601bb42f632e30ed469ca6059b582b
Ctibor Laky <luckylooke@gmail.com>
2017-05-31 11:03:01 +0200

    clean service codes

--- controllers/default.js
+++
@@ -1,8 +0,0 @@
-exports.install = function() {
-	F.route('/', plain_version);
-};
-
-function plain_version() {
-	var self = this;
-	self.plain('REST Service {0}\nVersion: {1}'.format(F.config.name, F.config.version));
-}
